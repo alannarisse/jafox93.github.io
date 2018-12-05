d1445dc0c4dc6cc294d905c322aae255792e89d8
Jackson Foxcroft <jacksonfoxcroft@Jacksons-MacBook-Pro-2.local>
2018-12-05 13:21:40 -0800

    initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    $('.slideshow').square1({
+        animation: 'slide',
+        transition_time: 2000,
+        prev_next_nav: 'hover',
+        dots_nav: 'hover',
+    });
+   
+
+
+
+
+    // don't delete this line
+});
